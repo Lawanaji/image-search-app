2e58c95f0f9bde452579c224c4e5b3c13eedd9bb
Lawan Mustapha Aji <101484231+Lawanaji@users.noreply.github.com>
2023-11-13 10:57:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,48 +14,48 @@ const data =inputEl.value;
 console.log(data)
 })
 
-// async function searchImages(){
-//     inputData = inputEl.value;
-//     const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id= ${accessKey}`
-//     const responce = await fetch(url)
-//     const data = await responce.json()
-//     const results = data.results
+async function searchImages(){
+    inputData = inputEl.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id= ${accessKey}`
+    const responce = await fetch(url)
+    const data = await responce.json()
+    const results = data.results
 
 
-//     if(page === 1){
-//         searchResults.innerHTML = "";
-//     }
-// results.map((result) =>{
-//     const imageWrapper = document.createElement('div')
-//     imageWrapper.classList.add("search-result")
-//     const image = document.createElement('imag')
-//     image.src = result.ulrs.small;
-//     image.alt = document.alt_decription;
-//     const imageLink = document.createElement('a')
-//     imageLink.target = "_blank";
-//     imageLink.textContent = result.alt_decription;
+    if(page === 1){
+        searchResults.innerHTML = "";
+    }
+results.map((result) =>{
+    const imageWrapper = document.createElement('div')
+    imageWrapper.classList.add("search-result")
+    const image = document.createElement('imag')
+    image.src = result.ulrs.small;
+    image.alt = document.alt_decription;
+    const imageLink = document.createElement('a')
+    imageLink.target = "_blank";
+    imageLink.textContent = result.alt_decription;
 
 
-//     imageWrapper.appendChild(image)
-//     image.appendChild(imageLink)
-//     searchResults.appendChild(imageWrapper)
+    imageWrapper.appendChild(image)
+    image.appendChild(imageLink)
+    searchResults.appendChild(imageWrapper)
 
-// })
+})
 
-// page++
-// if(page > 1){
-//     showMore.style.display = 'block';
-// }
+page++
+if(page > 1){
+    showMore.style.display = 'block';
+}
 
 
-// }
+}
 
-// formEl.addEventListener("submit", (event) =>{
-//     event.preventDefault(); 
-//     page = 1
-//     searchImages()
-// })
+formEl.addEventListener("submit", (event) =>{
+    event.preventDefault(); 
+    page = 1
+    searchImages()
+})
 
-// showMore.addEventListener("click", () =>{
-//     searchImages()
-// })
+showMore.addEventListener("click", () =>{
+    searchImages()
+})
